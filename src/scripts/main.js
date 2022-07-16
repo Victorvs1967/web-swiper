6f77efeafe6562cdc652b7066fb04bb6bde87ecc
Victor <victorsmirnov@me.com>
2022-07-16 21:22:08 +0300

    create swiper tutor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,49 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper from 'swiper/bundle';
 
+const swiper = new Swiper('.image-slider', {
+  // loop: true,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  autoHeight: true,
+  speed: 500,
+  effect: 'fade',
+
+  breakpoints: {
+    320: {
+      slidePerView: 1,
+    },
+    480: {
+      slidePerView: 2,
+    },
+    990: {
+      slidePerView: 3,
+    },
+  },
+
+  autoplay: {
+    delay: 1000,
+    stopOnLastSlide: false,
+    disableOnIteration: false,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    
+    type: 'fraction',
+    renderFraction: (currentClass, totalClass) => `photo <span class="${currentClass}"></span> of <span class="${totalClass}"></span>`,
+    
+    // clickable: true,
+    // dynamicBullets: true,
+    // renderBullet: (index, className) => `<span class="${className}">${index + 1}</span>`,
+    
+    // type: 'progressbar'
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  }
+});
