21e74583bcc46c684ea7f366d90435ca7f23d5ad
Victor <victorsmirnov@me.com>
2022-07-16 16:10:07 +0300

    start project

---
+++ gulp/config/path.js
@@ -0,0 +1,36 @@
+import * as nodePath from 'path';
+
+const root = nodePath.basename(nodePath.resolve());
+
+const src = './src',
+      dest = './dist';
+
+export const path = {
+  src: {
+    scripts: `${src}/scripts/main.js`,
+    styles: `${src}/styles/*.sass`,
+    html: `${src}/*.pug`,
+    images: `${src}/images/**/*.{png,jpg,jpeg,gif,webp}`,
+    svg: `${src}/images/**/*.svg`,
+    svgicons: `${src}/icons/*.svg`,
+  },
+  dest: {
+    scripts: `${dest}/js/`,
+    styles: `${dest}/css/`,
+    html: `${dest}/`,
+    images: `${dest}/images/`,
+    fonts: `${dest}/fonts/`
+  },
+  watch: {
+    scripts: `${src}/scripts/**/*.js`,
+    styles: `${src}/styles/**/*.sass`,
+    html: `${src}/**/*.pug`,
+    images: `${src}/images/**/*.{png,jpg,jpeg,gif,webp,svg}`,
+    svg: `${src}/images/**/*.svg`,
+    svgicons: `${src}/icons/*.svg`,
+},
+  clean: dest,
+  buildFolder: dest,
+  srcFolder: src,
+  rootFolder: root
+};
